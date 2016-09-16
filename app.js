a10c855363b51169d71d16e1917f1eea915ff6d1
Akiva Levy <akiva@sixthirteen.co>
2016-09-15 17:31:30 -0700

    Refactor, add styling
    Refactoring code, functionality, and file structure.
    
    Adding modular styles, using `sheetify`.

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const choo = require('choo')
 const html = require('choo/html')
 const app = choo()
+const sheetify = require('sheetify')
+
 let socket
+
+sheetify('css-wipe', { global: true })
+sheetify('css-type-base', { global: true })
+sheetify('ergonomic-breakpoint', { global: true })
 
 if (!module.parent) {
   const ws = require('websocket-stream')
@@ -9,19 +15,19 @@ if (!module.parent) {
   socket.on('error', console.error.bind(console, 'Error:'))
 }
 
-app.model(require('./models/user')(socket))
-app.model(require('./models/room')(socket))
+app.model(require('./lib/login/model')(socket))
+app.model(require('./lib/room/model')(socket))
 
 function auth (view) {
   return function (state, prev, send) {
     return state.user.name ?
       view(state, prev, send) :
-      require('./views/login')(state, prev, send)
+      require('./lib/login/view')(state, prev, send)
   }
 }
 
 app.router((route) => [
-  route('/', auth(require('./views/room'))),
+  route('/', auth(require('./lib/room/view'))),
 ])
 
 if (module.parent) module.exports = app
